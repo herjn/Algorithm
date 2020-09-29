8ad4c666bcbff9a0b77cc61637f931d17400b4c7
정인 <43091713+herjn@users.noreply.github.com>
2020-09-29 14:07:25 +0900

    Update H-index.py

--- 프로그래머스/정렬/H-index.py
+++ 프로그래머스/정렬/H-index.py
@@ -8,8 +8,11 @@ def solution(citations):
     return 0
 
 
+
 #다른사람 코드
 def solution(citations):
     citations.sort(reverse=True)
     answer = max(map(min, enumerate(citations, start=1)))
     return answer
+
+- 역순으로 뒤집어서 citations[i]와 i를 비교해서 작은 값을 선택
