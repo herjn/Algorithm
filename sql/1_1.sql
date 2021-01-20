d1fef025f7810adac3fe447e004acb4ac80c2742
herjn <leejungin199@naver.com>
2021-01-20 17:46:29 +0900

    sql update

---
+++ sql/1_1.sql
@@ -0,0 +1,7 @@
+# 모든 레코드 조회 문제
+/*SELECT * 를 통해 ANIMAL_INS의 모든 컬럼을 가져올 수 있게 된다.
+ANIMAL_ID 순으로 가져오라고 했으므로 FROM 아래에 
+ORDER BY ANIMAL_ID 를 넣어 ANIMAL_ID 순으로 가져오면 해결이다.*/
+
+SELECT * FROM ANIMAL_INS 
+ORDER BY ANIMAL_ID
