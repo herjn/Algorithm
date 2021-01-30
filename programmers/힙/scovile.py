a4dc4f7a0d87ce68e00822ced5c90e73b3aff105
herjn <leejungin199@naver.com>
2021-01-31 00:34:21 +0900

    update scovile.py

---
+++ programmers/힙/scovile.py
@@ -0,0 +1,17 @@
+import heapq
+def solution(scoville, K):
+    answer = -1
+    count = 0
+    check_flag = False
+    heapq_scoville = heapq.heapify(scoville)
+    while scoville[0] < K:
+        min_first = heapq.heappop(scoville)
+        min_second = heapq.heappop(scoville)
+        heapq.heappush(scoville, min_first + (min_second * 2))
+        if len(scoville) == 1 and scoville[0] < K:
+            check_flag = True
+            break
+        count = count + 1
+    if check_flag == False: 
+        answer = count
+    return answer
