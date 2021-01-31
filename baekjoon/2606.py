a1e7b0bc47bffef1d81f098dbcabb634b40fec19
herjn <leejungin199@naver.com>
2021-01-31 20:38:29 +0900

    update 2606.py

--- baekjoon/2606.py
+++ baekjoon/2606.py
@@ -1,3 +1,4 @@
+#DFS로 풀기
 import sys
 input = sys.stdin.readline
 com=int(input()); con=int(input())
@@ -18,4 +19,28 @@ def dfs(start, dic):
             
 visited = []
 dfs(1, dic)
+print(len(visited)-1)
+
+#BFS로 풀기
+import sys
+input = sys.stdin.readline
+com=int(input()); con=int(input())
+dic={}
+
+for i in range(com):
+    dic[i+1] = set()
+for j in range(con):
+    a, b = map(int,input().split())
+    dic[a].add(b)
+    dic[b].add(a)
+
+def bfs(start, dic):
+    queue = [start]
+    while queue:
+        for i in dic[queue.pop()]:
+            if i not in visited:
+                visited.append(i)
+                queue.append(i)
+visited = []
+bfs(1, dic)
 print(len(visited)-1)
