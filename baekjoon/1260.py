2cf4210a7d4ac45ea2a27b982db6dcbe1686db6c
herjn <leejungin199@naver.com>
2021-02-07 14:34:34 +0900

    edit 1260.py

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,3 +1,4 @@
+#인접행렬로 그래프 구현
 n,m,v=map(int,input().split())
 matrix=[[0]*(n+1) for _ in range(n+1)]
 visited=[False]*(n+1)
@@ -27,3 +28,45 @@ def bfs(v):
 dfs(v)
 print()
 bfs(v)
+
+
+#인접리스트로 그래프 구현
+import sys 
+input = sys.stdin.readline 
+from collections import deque 
+
+def dfs(graph, v):
+    visited = []
+    stack = [v]
+    while stack:
+        n = stack.pop() 
+        if n not in visited:
+            visited.append(n)
+            stack += reversed(graph[n])
+            return visited
+
+def bfs(graph, v):
+    visited = []
+    queue = deque([v])
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            queue += graph[n]
+            return visited
+            
+n,m,v = map(int, input().split())
+graph = {i:[] for i in range(1,n+1)}
+
+for i in range(1, m+1):
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+for key in graph:
+    graph[key].sort()
+
+
+print(' '.join(list(map(str,dfs(graph, v)))))
+
+print(' '.join(list(map(str,bfs(graph, v)))))
