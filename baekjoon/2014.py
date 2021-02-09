2bb8e4397497684f5fb4671db16f390ad644b175
herjn <leejungin199@naver.com>
2021-02-10 00:08:13 +0900

    update 2014.py

---
+++ baekjoon/2014.py
@@ -0,0 +1,20 @@
+import heapq
+
+if __name__ == '__main__':
+    n,k=map(int,input().split())
+    prime=list(map(int,input().split()))
+
+    pq = []
+    for num in prime:
+        heapq.heappush(pq,num)
+
+    for i in range(n): 
+        num = heapq.heappop(pq)
+        for j in range(k): 
+            new_num = num * prime[j]
+            heapq.heappush(pq, new_num)
+
+            if num%prime[j] == 0:  
+                break
+    else:
+        print(num)
