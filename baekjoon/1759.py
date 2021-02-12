bd5e42e3c41429e4267af92feb7faa1ebed5f165
herjn <leejungin199@naver.com>
2021-02-12 15:28:47 +0900

    update 1759.py

---
+++ baekjoon/1759.py
@@ -0,0 +1,18 @@
+import sys
+from itertools import combinations
+
+l,c=map(int, input().split())
+pwd=list(map(str, input().split()))
+pwd.sort()
+
+alpha=['a','e','i','o','u']
+
+com=combinations(pwd,l)
+for i in com:
+    cnt=0
+    for j in i:
+        if j in alpha:
+            cnt+=1
+
+    if cnt>=1 and cnt<=l-2:
+        print(''.join(i))
