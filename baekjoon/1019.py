894f0eca2b15ede2d34be93c040310710a3d2900
herjn <leejungin199@naver.com>
2021-02-12 16:12:17 +0900

    update 1019.py

---
+++ baekjoon/1019.py
@@ -0,0 +1,22 @@
+n=int(input())
+num=[0 for i in range(10)]
+point=1
+
+while n!=0:
+    while n%10!=9:
+        for i in str(n):
+            num[int(i)]+=point
+        n-=1
+    if n<10:
+        for i in range(n+1):
+            num[i]+=point
+        num[0]-=point
+        break
+    else:
+        for i in range(10):
+            num[i]+=(n//10+1)*point
+    num[0]-=point
+    point*=10
+    n//=10
+for i in s:
+    print(i,end=' ')
