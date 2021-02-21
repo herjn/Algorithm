197777dc9d25a2fae26737501f498ee62ad54ffe
herjn <leejungin199@naver.com>
2021-02-21 17:50:08 +0900

    update 4395.py

---
+++ baekjoon/4395.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+def fact(n):
+    a=1
+    for i in range(1,n+1):
+        a*=i
+    return a
+
+def solution():
+    n=int(sys.stdin.readline()) 
+    for _ in range(n):
+        x,y=map(int,input().split(' '))
+        distance=y-x
+        i=1
+        while i*i<distance:
+            i+=1
+        if distance<(i*i)-i+1:
+            print(2*i-2)
+        else:
+            print(2*i-1)
+solution()
