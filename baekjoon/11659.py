5a5475ef7cd8b0865b1de97ee2d6f7731170b4fd
herjn <leejungin199@naver.com>
2021-02-26 17:22:28 +0900

    update 11659.py

---
+++ baekjoon/11659.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+table=list(map(int,input().split()))
+sum=[0]
+for i in range(n):
+    sum.append(sum[-1]+table[i])
+
+for _ in range(m):
+    x,y=map(int,input().split())
+    if x==1:
+        print(sum[y])
+    else:
+        print(sum[y]-sum[x-1])
+
